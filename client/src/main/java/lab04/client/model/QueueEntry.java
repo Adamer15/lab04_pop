df2b5f9241db87d0e4186befd5d32902388aef7e
Adamer15 <117001767+Adamer15@users.noreply.github.com>
2024-12-02 23:28:59 +0100

    Dodanie metod. Więcej działa

--- client/src/main/java/lab04/client/model/QueueEntry.java
+++ client/src/main/java/lab04/client/model/QueueEntry.java
@@ -5,55 +5,121 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class QueueEntry {
-    @JsonProperty("provider") // Nazwa świadczeniodawcy
-    private String provider;
 
-    @JsonProperty("benefit") // Nazwa usługi/świadczenia
-    private String service;
+    @JsonProperty("type")
+    private String type;
 
-    @JsonProperty("waitingTime") // Czas oczekiwania w dniach
-    private int waitingTime;
+    @JsonProperty("id")
+    private String id;
 
-    @JsonProperty("queueLength") // Długość kolejki w osobach
-    private int queueLength;
+    @JsonProperty("attributes")
+    private Attributes attributes;
 
-    public QueueEntry() {}
-
-    public String getProvider() {
-        return provider;
+    public String getType() {
+        return type;
     }
 
-    public void setProvider(String provider) {
-        this.provider = provider;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public String getService() {
-        return service;
+    public String getId() {
+        return id;
     }
 
-    public void setService(String service) {
-        this.service = service;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public int getWaitingTime() {
-        return waitingTime;
+    public Attributes getAttributes() {
+        return attributes;
     }
 
-    public void setWaitingTime(int waitingTime) {
-        this.waitingTime = waitingTime;
-    }
-
-    public int getQueueLength() {
-        return queueLength;
-    }
-
-    public void setQueueLength(int queueLength) {
-        this.queueLength = queueLength;
+    public void setAttributes(Attributes attributes) {
+        this.attributes = attributes;
     }
 
     @Override
     public String toString() {
         return String.format("Placówka: %s, Usługa: %s, Czas oczekiwania: %d dni, Długość kolejki: %d osób",
-                provider, service, waitingTime, queueLength);
+                attributes.getProvider(),
+                attributes.getBenefit(),
+                attributes.getStatistics() != null ? attributes.getStatistics().getProviderData().getAveragePeriod() : 0,
+                attributes.getStatistics() != null ? attributes.getStatistics().getProviderData().getAwaiting() : 0);
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Attributes {
+        @JsonProperty("provider")
+        private String provider;
+
+        @JsonProperty("benefit")
+        private String benefit;
+
+        @JsonProperty("statistics")
+        private Statistics statistics;
+
+        public String getProvider() {
+            return provider;
+        }
+
+        public void setProvider(String provider) {
+            this.provider = provider;
+        }
+
+        public String getBenefit() {
+            return benefit;
+        }
+
+        public void setBenefit(String benefit) {
+            this.benefit = benefit;
+        }
+
+        public Statistics getStatistics() {
+            return statistics;
+        }
+
+        public void setStatistics(Statistics statistics) {
+            this.statistics = statistics;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Statistics {
+        @JsonProperty("provider-data")
+        private ProviderData providerData;
+
+        public ProviderData getProviderData() {
+            return providerData;
+        }
+
+        public void setProviderData(ProviderData providerData) {
+            this.providerData = providerData;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class ProviderData {
+        @JsonProperty("awaiting")
+        private int awaiting;
+
+        @JsonProperty("average-period")
+        private int averagePeriod;
+
+        public int getAwaiting() {
+            return awaiting;
+        }
+
+        public void setAwaiting(int awaiting) {
+            this.awaiting = awaiting;
+        }
+
+        public int getAveragePeriod() {
+            return averagePeriod;
+        }
+
+        public void setAveragePeriod(int averagePeriod) {
+            this.averagePeriod = averagePeriod;
+        }
     }
 }
