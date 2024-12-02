897d10e7aa53e00563cf310393bcb64b74d16d8d
Adamer15 <117001767+Adamer15@users.noreply.github.com>
2024-12-02 21:37:52 +0100

    Dodanie szkieletu projektu

---
+++ client/src/main/java/lab04/NFZClient.java
@@ -0,0 +1,7 @@
+package lab04;
+
+public class NFZClient {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ client/src/main/java/lab04/model/QueueEntry.java
@@ -0,0 +1,4 @@
+package lab04.model;
+
+public class QueueEntry {
+}
